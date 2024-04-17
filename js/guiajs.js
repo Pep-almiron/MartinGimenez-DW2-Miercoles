00b5ae61f6a5d0e9ed7f66db2cddeaf9b43f967e
pep-almiron <gimenezmartinalejandro83@gmaiil.com>
2024-04-17 19:45:14 -0300

    js

---
+++ js/guiajs.js
@@ -0,0 +1,33 @@
+/* introduccion */
+
+console.log("hola"); /*mensaje por teminal*/
+
+prompt("hola"); /*ventana emergente y campo*/
+
+alert("hola jaja"); /*alerta en ventana emergente*/
+
+let /*asignar variable*/ edad /*nombre de la variable*/ = "16" /*variable*/
+
+const /*dato que no se pude cambiar*/ nombre = "martin"
+
+///////////////////////////////////////////////////////////////////
+
+/* concatenar variables*/
+
+const name = "Martin"
+let apellido = "Gimenez"
+
+console.log ("nombre" + " " + "apellido") /*unir variables*/
+
+console.log(`mi nombre es ${nombre} ${apellido}`) /*mejor manera*/
+
+////////////////////////////////////////////////////////////////////
+
+/* operacion suma y resta*/
+
+let añousuario = prompt ("¿en que año naciste?")
+
+let edadusuario = 2024 - añousuario
+
+alert ("Tu Edad es: " + edadusuario )
+
